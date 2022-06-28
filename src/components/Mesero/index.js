d92a07554144084bb0c1606aaf165736fed02909
Angie Mariscal <amariscalp12@gmail.com>
2022-06-28 15:58:00 -0500

    ultimos cmabios

--- src/components/Mesero/index.js
+++ src/components/Mesero/index.js
@@ -1,9 +1,54 @@
-import { collection, getDocs, addDoc } from "firebase/firestore";
+import { collection, getDocs, addDoc , where, query} from "firebase/firestore";
 import {db} from "../../firebase/connection.js"
 import {useEffect, useState} from 'react'
 import './style.css'
 
+import ItemCard from './pruebaBoost.js'
 
+
+export default function Orders(){
+    const [total, setTotal] = useState([]);
+    const getBreakFast = async () => {
+    const data = await getDocs(query(collection(db,'menu'), where("type", "==", "bebida")))
+
+    setTotal(data.docs.map((doc) => ({...doc.data(),id:doc.id}))
+  )
+ }
+////////////////////////////////////////////////////
+const usersCollectionRefDos = collection(db,"orden");
+ const showItemsBreakfast = async() =>{
+    // await addDoc (usersCollectionRefDos,{product:precio, price:producto})
+    await addDoc (usersCollectionRefDos,{product:total })
+ };
+//////////////////////////////////////////////////
+    useEffect(() => {
+     getBreakFast()
+    }, []);
+     return (
+         <div id="ordersViewContainer">
+             <h1 className="text-center mt-3">hola</h1>
+             <section className="py-4 container">
+                 <div className=" row justify-content-center">
+                 {total.map((item )=>{
+                     console.log("dos")
+                     console.log(item)
+                     return(
+                        <ItemCard image={item.image} product={item.product} price={item.price} id={item.id} ></ItemCard>   
+                     )
+                 })}
+                 </div>
+             </section>
+    </div>
+)
+}
+
+
+
+
+
+//<button onClick={showItemsBreakfast} className="buttonOrders">Desayuno</button>
+
+/*
 export default function Orders(){
     //const [data, setData] = useState(menu.breakfast);
     //const [order, setOrder] = useState([]);
@@ -23,15 +68,15 @@ const usersCollectionRefDos = collection(db,"orden");
     useEffect(() => {
      getBreakFast()
     }, []);
-return (
-   
-    <div id="ordersViewContainer">
-         <div>
-          <button type="button" className="btn btn-primary">Primary</button>
-          <button type="button" className="btn btn-secondary">Secondary</button>
-          <button type="button" className="btn btn-success">Success</button>
-      </div>
-        <div id="ordersMenuOptions">
+     return (
+         <div id="ordersViewContainer">
+          <div>
+           <button type="button" className="btn btn-primary">Primary</button>
+           <button type="button" className="btn btn-secondary">Secondary</button>
+           <button type="button" className="btn btn-success">Success</button>
+          </div>
+
+          <div id="ordersMenuOptions">
             {total.map(item => (
                 <div key={item.id}>
 
@@ -46,4 +91,4 @@ return (
     </div>
 )
 }
-//<button onClick={showItemsBreakfast} className="buttonOrders">Desayuno</button>
+*/
--- src/components/Mesero/pruebaBoost.js
+++ src/components/Mesero/pruebaBoost.js
@@ -1,12 +1,21 @@
 import React from 'react'
 
-const PruebaUno = () => {
+const ItemCard = (props) => {
+  console.log("as")
+  console.log(props)
     return( 
-      <div>
-          <button type="button" className="btn btn-primary">Primary</button>
-          <button type="button" className="btn btn-secondary">Secondary</button>
-          <button type="button" className="btn btn-success">Success</button>
+      <div className='col-11 col-md-6 col-lg-3 mx-0 mb-4'>
+        <div className='card p-0 overflow-hidden h-100 shadow'>
+          <img src={props.image} className='card-img-top img-fluid'/>
+          <div className='card-body'>
+          <div id={props.id}>
+            <h5 className='card-title'>{props.product} </h5>
+            <p className='card-text'>{props.price} </p>
+            <button className='btn btn-success'>Agregar</button>
+            </div>
+          </div>
+        </div>
       </div>
     );
 }
-export default PruebaUno;
+export default ItemCard;
