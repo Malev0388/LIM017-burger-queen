12fd4678237119a478e935c2bc932fbdd3c257ca
malev.dg <malev.dg@outlook.com>
2022-06-28 14:36:00 -0500

    no hacer caso 7

--- src/components/Mesero/index.js
+++ src/components/Mesero/index.js
@@ -1,49 +1,100 @@
-import { collection, getDocs, addDoc } from "firebase/firestore";
-import {db} from "../../firebase/connection.js"
-import {useEffect, useState} from 'react'
-import './style.css'
+import React, { useEffect, useState } from "react";
+import "./style.css";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../firebase/connection.js";
 
+export default function Index(props) {
+  const [total, setTotal] = useState([]);
+  //const showItemsBreakfast = () => {
+  //return setData(menu.breakfast)
+  const getBreakFast = async () => {
+    const product = await getDocs(collection(db, "menu"));
+    setTotal(product.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+  };
+  useEffect(() => {
+    getBreakFast();
+  }, []);
+  return (
+    <div className="mesero">
+      <header className="header">
+        <img
+          src="https://firebasestorage.googleapis.com/v0/b/burger-queen-d0e74.appspot.com/o/logo-bq.PNG?alt=media&token=ff5fc092-8e35-45fc-ab81-6b4a4a7efa45"
+          alt="logo-bq"
+          className="logo-bq"
+        />
+      </header>
+      <div className="buttons-mesero">
+        <div className="button-desayuno"> Desayuno </div>
+        <div className="button-menu"> Menu </div>
+        <div className="button-pedidos"> Pedidos Listos </div>
+      </div>
+      <div className="container-mesero">
+        <div className="container-menu">
+        {total.map((item) => (
+            <div className="item-product" key={item.id}>
+          <img
+            className="img-menu"
+            src={item.image}
+            alt="img-menu"
+          />
+          <div className="container-text-menu">
+            <p className="name-product"> {item.product} </p>
+            <p className="precio"> S/. {item.price} </p>
+          </div>
+          </div>
+          ))}
+        </div>
+        <div className="car">
+          <p> Esta es la parte del carrito </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+/*import menu from "../../menu.json"
+import {useState} from 'react'
 
 export default function Orders(){
-    //const [data, setData] = useState(menu.breakfast);
+    const [data, setData] = useState(menu.breakfast);
     //const [order, setOrder] = useState([]);
-    const [total, setTotal] = useState([]);
-    const getBreakFast = async () => {
-    const data = await getDocs(collection(db,'menu'));
-    setTotal(data.docs.map((doc) => ({...doc.data(),id:doc.id}))
-  )
- }
-
-const usersCollectionRefDos = collection(db,"orden");
- const showItemsBreakfast = async() =>{
-    // await addDoc (usersCollectionRefDos,{product:precio, price:producto})
-    await addDoc (usersCollectionRefDos,{product:total})
- };
-
-    useEffect(() => {
-     getBreakFast()
-    }, []);
+    //const [total, setTotal] = useState([]);
+ 
+    
+const showItemsBreakfast = () => {
+    return setData(menu.breakfast)
+};
 return (
-   
     <div id="ordersViewContainer">
-         <div>
-          <button type="button" className="btn btn-primary">Primary</button>
-          <button type="button" className="btn btn-secondary">Secondary</button>
-          <button type="button" className="btn btn-success">Success</button>
-      </div>
         <div id="ordersMenuOptions">
-            {total.map(item => (
-                <div key={item.id}>
-
-            <img src={item.image} alt='' width="320" height="300"/>
-            
-            <h1>{item.product}</h1>
-            <h3>{item.price}</h3>
-            <button onClick={showItemsBreakfast} className="buttonOrders">Agregar</button>
-            </div>
-            ))}
+            <button onClick={showItemsBreakfast} className="buttonOrders"> Desayuno</button>
         </div>
     </div>
 )
-}
-//<button onClick={showItemsBreakfast} className="buttonOrders">Desayuno</button>
+}*/
+/*import React, { useState, useEffect } from 'react'
+
+import importBreakfast from '../../firebase/menu.js'
+import { collection, query, where, onSnapshot } from "firebase/firestore";
+import {db} from "../../firebase/connection.js"
+
+//import {Link} from 'react-router-dom'
+
+/*export default function Index(){
+    const [menu, setMenu] = useState([]);
+
+    useEffect(() => {  
+    const breakfast = query(collection(db, "menu"), where("type", "==", "breakfast"));
+    const importBreakfast = onSnapshot(breakfast, (snapshot) => 
+        setMenu(snapshot.docs.map(producto =>({...producto.data()}))))
+    return importBreakfast
+    }, []);
+
+    console.log(menu);
+
+
+    return(
+        <div>Mesero</div>
+
+    )
+}*/
--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -4,22 +4,12 @@ import './style.css'
 
 export default function index(){
     return(
-        <div className="border m-4 d-flex justify-content-center align-items-center ">
-        <div className="position-absolute top-50 justify-content-space-between ">
-        <Link to='/Mesero' className="btn btn-secondary">Mesero</Link>
-        <Link to='/Chef' className="btn btn-secondary">Chef</Link>
-        <Link to='/Admin' className="btn btn-success">Admin</Link>
+        <div className='border m-4 d-flex justify-content-center align-items-center'>
+        <div className='position-absolute top-50 justify-content-space-between'>
+        <Link to='/Mesero' className='btn btn-secondary'>Mesero</Link>
+        <Link to='/Chef' className='btn btn-secondary'>Chef</Link>
+        <Link to='/Admin' className='btn btn-success'>Admin</Link>
         </div>
         </div>
-    )   
+    )
 }
-
-/*
- <div className="border m-4 d-flex justify-content-center align-items-center ">
-        <div className="position-absolute top-50 justify-content-space-between ">
-        <Link to='/Mesero' className='btn btn-secondary '>Mesero</Link>
-        <Link to='/Chef' className="btn btn-secondary">Chef</Link>
-        <Link to='/Admin' className="btn btn-success">Admin</Link>
-        </div>
-        </div>
-*/
