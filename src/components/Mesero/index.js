ca7f7de075e07917dda20c8988bd8aeec2146747
malev.dg <malev.dg@outlook.com>
2022-06-28 10:49:47 -0500

    no hacer caso 3

--- src/components/Mesero/index.js
+++ src/components/Mesero/index.js
@@ -1,38 +1,3 @@
-<<<<<<< HEAD
-import { collection, getDocs, addDoc } from 'firebase/firestore';
-import {db} from '../../firebase/connection.js'
-import {useEffect, useState} from 'react'
-export default function Orders(){
-    //const [data, setData] = useState(menu.breakfast);
-    //const [order, setOrder] = useState([]);
-    const [total, setTotal] = useState([]);
-    const getBreakFast = async () => {
-    const data = await getDocs(collection(db,'menu'));
-    setTotal(data.docs.map((doc) => ({...doc.data(),id:doc.id}))
-  )
- }
-//////////////////////////
-const usersCollectionRefDos = collection(db,'orden');
- const showItemsBreakfast = async() =>{
-    // await addDoc (usersCollectionRefDos,{product:precio, price:producto})
-    await addDoc (usersCollectionRefDos,{product:total })
- };
-////////////////////////
-    useEffect(() => {
-     getBreakFast()
-    }, []);
-return (
-    <div id='ordersViewContainer'>
-        <div id='ordersMenuOptions'>
-            {total.map(item => (
-                <div key={item.id}>
-            <img src={item.image} alt=''/>
-            <h1>{item.product}</h1>
-            <h3>{item.price}</h3>
-            <button onClick={showItemsBreakfast} className='buttonOrders'>Agregar</button>
-            </div>
-            ))}
-=======
 import menu from "../../menu.json"
 import {useEffect, useState, useContext} from 'react'
 
@@ -52,16 +17,10 @@ return (
         </div>
         <div id="elementsOdersView">
             <h1> data={data} </h1>
->>>>>>> a20514d7714ad764b582c718dd757b35cc889cfc
         </div>
     </div>
 )
 }
-<<<<<<< HEAD
-//<button onClick={showItemsBreakfast} className=“buttonOrders”>Desayuno</button>
-
-=======
->>>>>>> a20514d7714ad764b582c718dd757b35cc889cfc
 /*import React, { useState, useEffect } from 'react'
 
 import importBreakfast from '../../firebase/menu.js'
