4dd41093819269f1f176312385db1b1acac73fb6
malev.dg <malev.dg@outlook.com>
2022-06-28 12:32:17 -0500

    no hacer caso 6

--- src/components/Mesero/index.js
+++ src/components/Mesero/index.js
@@ -13,13 +13,13 @@ export default function Orders(){
     setTotal(data.docs.map((doc) => ({...doc.data(),id:doc.id}))
   )
  }
-////////////////////////////////////////////////////
+
 const usersCollectionRefDos = collection(db,"orden");
  const showItemsBreakfast = async() =>{
     // await addDoc (usersCollectionRefDos,{product:precio, price:producto})
-    await addDoc (usersCollectionRefDos,{product:total })
+    await addDoc (usersCollectionRefDos,{product:total})
  };
-//////////////////////////////////////////////////
+
     useEffect(() => {
      getBreakFast()
     }, []);
@@ -47,52 +47,3 @@ return (
 )
 }
 //<button onClick={showItemsBreakfast} className="buttonOrders">Desayuno</button>
-import menu from "../../menu.json"
-import {useEffect, useState, useContext} from 'react'
-
-export default function Orders(){
-    const [data, setData] = useState(menu.breakfast);
-    const [order, setOrder] = useState([]);
-    const [total, setTotal] = useState([]);
- 
-    
-const showItemsBreakfast = () => {
-    return setData(menu.breakfast)
-};
-return (
-    <div id="ordersViewContainer">
-        <div id="ordersMenuOptions">
-            <button onclick={showItemsBreakfast} classname="buttonOrders"> Desayuno</button>
-        </div>
-        <div id="elementsOdersView">
-            <h1> data={data} </h1>
-        </div>
-    </div>
-)
-}
-/*import React, { useState, useEffect } from 'react'
-
-import importBreakfast from '../../firebase/menu.js'
-import { collection, query, where, onSnapshot } from "firebase/firestore";
-import {db} from "../../firebase/connection.js"
-
-//import {Link} from 'react-router-dom'
-
-/*export default function Index(){
-    const [menu, setMenu] = useState([]);
-
-    useEffect(() => {  
-    const breakfast = query(collection(db, "menu"), where("type", "==", "breakfast"));
-    const importBreakfast = onSnapshot(breakfast, (snapshot) => 
-        setMenu(snapshot.docs.map(producto =>({...producto.data()}))))
-    return importBreakfast
-    }, []);
-
-    console.log(menu);
-
-
-    return(
-        <div>Mesero</div>
-
-    )
-}*/
