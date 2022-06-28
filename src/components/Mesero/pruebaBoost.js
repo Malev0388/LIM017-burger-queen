d527c190fbe37592f0576a568d04ba949403b6bd
Angie Mariscal <amariscalp12@gmail.com>
2022-06-28 17:06:17 -0500

    descargando cambios

--- src/components/Mesero/pruebaBoost.js
+++ src/components/Mesero/pruebaBoost.js
@@ -4,7 +4,31 @@ const ItemCard = (props) => {
   console.log("as")
   console.log(props)
     return( 
-      <div className='col-11 col-md-6 col-lg-3 mx-0 mb-4'>
+    
+      <div className="container-mesero">
+        <div className="container-menu">
+            <div className="item-product" id={props.id}>
+          <img
+            className="img-menu"
+            src={props.image}
+            alt="img-menu"
+          />
+          <div className="container-text-menu">
+            <p className="name-product"> {props.product} </p>
+            <p className="precio"> S/. {props.price} </p>
+          </div>
+          </div>
+        </div>
+        
+        
+      </div>
+      
+    );
+}
+export default ItemCard;
+
+/*
+ <div className='col-11 col-md-6 col-lg-3 mx-0 mb-4'>
         <div className='card p-0 overflow-hidden h-100 shadow'>
           <img src={props.image} className='card-img-top img-fluid'/>
           <div className='card-body'>
@@ -16,6 +40,4 @@ const ItemCard = (props) => {
           </div>
         </div>
       </div>
-    );
-}
-export default ItemCard;
+*/
