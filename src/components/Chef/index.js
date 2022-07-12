8017888a652adbbea88efb5d8ffa971f71f69092
malev.dg <malev.dg@outlook.com>
2022-07-12 13:14:53 -0500

    vista chef

--- src/components/Chef/index.js
+++ src/components/Chef/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { collection, getDocs, query, where, getFirestore,doc, getDoc } from "firebase/firestore";
 import { db } from "../../firebase/connection.js";
+import "./style.css";
 
 
 export const Chef = () => {
@@ -19,9 +20,25 @@ export const Chef = () => {
 
   return (
     <div>
+      <header className="header">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/burger-queen-d0e74.appspot.com/o/logo-bq.PNG?alt=media&token=ff5fc092-8e35-45fc-ab81-6b4a4a7efa45"
+            alt="logo-bq"
+            className="logo-bq"
+          />
+      </header>
+      <ul >
+        <text className = "stateOrdersTittle">ESTADO DE PEDIDOS</text>
+          <div className="stateOrders">
+            <text className = "redBall">🔴 En proceso</text>
+            <text className = "greenBall">🟢 Pediente</text>
+          </div>
+      </ul>
+
       {users.map((user, id) => {
         // console.log("hola",user);
-        return (
+        return (           
+
           <div key={id}>
             <h1>nombre: {user.nameClient} </h1>
             <h1>mesa: {user.numberClient} </h1>
