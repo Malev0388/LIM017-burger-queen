2a7eb2dfb364ee5c615e2a47ece77b98c2583722
CarmenMerino <98608303+CarmenMerino@users.noreply.github.com>
2022-07-12 16:23:56 -0500

    css chef

--- src/components/Chef/index.js
+++ src/components/Chef/index.js
@@ -6,7 +6,7 @@ import "./style.css";
 
 export const Chef = () => {
 
-  /*const [users, setTotal] = useState([]);
+  const [users, setTotal] = useState([]);
   const usersCollectionRef = collection(db, "ordenes");
   const getUsers = async () => {
     const data = await getDocs(usersCollectionRef);
@@ -39,21 +39,19 @@ export const Chef = () => {
         // console.log("hola",user);
         return (           
 
-          <div key={id}>
-            <h1>nombre: {user.nameClient} </h1>
-            <h1>mesa: {user.numberClient} </h1>
-            <h1>comentario: {user.comentOrder} </h1>
+          <div className="container-order-chef" key={id}>
+            <div className="number-table">Número de mesa: {user.numberClient} </div>
             <div>
               {user.productCar.map ((product, id) => {
                 return (
-                  <div key={id}>
-                    <h1>Producto:{product.product}</h1>
-                    <h1>cantidad:{product.quantity}</h1>
+                  <div className="list-order-chef" key={id}>
+                    <h1 className="product-order">{product.product}</h1>
+                    <h1 className="quantify-order">({product.quantity})</h1>
                   </div>
                 )
               })}     
             </div>
-            <br />
+            <h1>comentario: {user.comentOrder} </h1>
           </div>
           //
         );
@@ -61,8 +59,7 @@ export const Chef = () => {
      
     </div>
   );
-};*/
-}
+};
 
 export default Chef;
 
